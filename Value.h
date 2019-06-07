f3b5b600408e4e3482e9d134f40da26481925eae
zhengkw18 <49091153+zhengkw18@users.noreply.github.com>
2019-06-07 22:35:05 +0800

    add date,time

--- Value.h
+++ Value.h
@@ -7,7 +7,7 @@ using namespace std;
 class Value;
 using ptr_v = shared_ptr<Value>;
 
-enum OprdType { Null, Zero, NZero, Double, Char };
+enum OprdType { Null, Zero, NZero, Double, Char, Date, Time};
 
 constexpr int total_type = Char + 1;
 
@@ -24,6 +24,7 @@ public:
     static ptr_v v_not(ptr_v);
     static ptr_v v_and(ptr_v, ptr_v);
     static ptr_v v_or(ptr_v, ptr_v);
+    static ptr_v v_xor(ptr_v, ptr_v);
     static ptr_v v_lt(ptr_v, ptr_v);
     static ptr_v v_gt(ptr_v, ptr_v);
     static ptr_v v_eq(ptr_v, ptr_v);
@@ -42,6 +43,9 @@ public:
     OprdType oprd_type() const override;
 	bool isnull() override { return false; }
 };
+
+class DateValue;
+class TimeValue;
 
 class IntValue : public Value{
     int value;
@@ -62,6 +66,8 @@ class CharValue : public Value{
     ostream& print(ostream&) const override;
 public:
     CharValue(string);
+    shared_ptr<DateValue> to_date() const;
+    shared_ptr<TimeValue> to_time() const;
 	string get_val() const{ return value; }
     bool operator<(const Value&) const override;
 	bool operator==(const Value& v) const override { return !(*this < v) && !(v < *this); }
@@ -70,3 +76,33 @@ public:
 	bool isnull() override { return false; }
 };
 
+class DateValue : public Value{
+    int year,month,day;
+    ostream& print(ostream&) const override;
+    void setzero();
+    bool isValid();
+public:
+    DateValue(string);
+    string get_formated() const{ string s; s+=to_string(year); s+="-"; s+=to_string(month); s+="-"; s+=to_string(day); return s;}
+    bool operator<(const Value&) const override;
+	bool operator==(const Value& v) const override { return !(*this < v) && !(v < *this); }
+    ptr_v cast_up(ptr_v) const override;
+    OprdType oprd_type() const override;
+    bool isnull() override { return false; }
+};
+
+class TimeValue : public Value{
+    int hour,minute,second;
+    bool positive=true;
+    ostream& print(ostream&) const override;
+    void setzero();
+    bool isValid();
+public:
+    TimeValue(string);
+    string get_formated() const{ string s=positive?"":"-"; s+=to_string(hour); s+=":"; s+=to_string(minute); s+=":"; s+=to_string(second); return s;}
+    bool operator<(const Value&) const override;
+	bool operator==(const Value& v) const override { return !(*this < v) && !(v < *this); }
+    ptr_v cast_up(ptr_v) const override;
+    OprdType oprd_type() const override;
+    bool isnull() override { return false; }
+};
